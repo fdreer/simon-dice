0c010d5884f929f0a13a2045d770f7af16ccb67e
Franco Dreer <francodreer@gmail.com>
2023-12-28 18:05:53 -0300

    add footer to page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css'
+import Footer from './components/Footer'
 import Board from './components/game/Board'
 
 const App = () => {
   return (
-    <main style={{ height: '100dvh', paddingTop: '45px' }}>
-      <h1>Simon Dice</h1>
-      <Board />
-    </main>
+    <>
+      <main>
+        <h1>Simon Dice</h1>
+        <Board />
+      </main>
+      <Footer />
+    </>
   )
 }
 
